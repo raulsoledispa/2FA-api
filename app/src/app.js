d08b8b6e54812a42657ca261eb3a96b5ce07b71d
Raul <soledispa.raul@gmail.com>
2021-08-06 11:52:10 -0500

    feat(app.js): Open API spec added

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,26 @@
-console.log('Initialzed');
+'use strict';
+import { fastify } from 'fastify';
+import fastifySwagger from 'fastify-swagger';
+import route from './controller.js';
+const app = fastify({ logger: true });
+
+app.register(fastifySwagger, {
+  mode: 'static',
+  specification: {
+    path: './app/src/docs/2FA.yaml',
+  },
+  exposeRoute: true,
+});
+
+app.register(route);
+
+const start = async () => {
+  try {
+    await app.listen(3000, '0.0.0.0');
+  } catch (error) {
+    app.log.error(error);
+    process.exit(1);
+  }
+};
+
+start();
---
+++ app/src/controller.js
@@ -0,0 +1,6 @@
+'use strict';
+export default async function routes(fastify, options) {
+  fastify.post('/2FA/api/generate', async (request, reply) => {
+    const { userId } = request.body;
+  });
+}
